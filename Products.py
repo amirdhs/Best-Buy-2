a49b79af271b6062e61206155697d58afe25d1bd
amirdehestani <amirmo800@gmail.com>
2025-01-19 18:11:17 +0100

    Move promotion logic to a separate file

--- Products.py
+++ Products.py
@@ -1,40 +1,4 @@
-from abc import ABC, abstractmethod
-
-# Abstract Promotion Class
-class Promotion(ABC):
-    def __init__(self, name):
-        self.name = name
-
-    @abstractmethod
-    def apply_promotion(self, product, quantity):
-        pass
-
-
-# Second Item at Half Price Promotion
-class SecondHalfPrice(Promotion):
-    def apply_promotion(self, product, quantity):
-        pairs = quantity // 2
-        remaining = quantity % 2
-        return (pairs * 1.5 * product.price) + (remaining * product.price)
-
-
-# Buy 2, Get 1 Free Promotion
-class ThirdOneFree(Promotion):
-    def apply_promotion(self, product, quantity):
-        groups_of_three = quantity // 3
-        remaining = quantity % 3
-        return (groups_of_three * 2 * product.price) + (remaining * product.price)
-
-
-# Percentage Discount Promotion
-class PercentDiscount(Promotion):
-    def __init__(self, name, percent):
-        super().__init__(name)
-        self.percent = percent
-
-    def apply_promotion(self, product, quantity):
-        discount = self.percent / 100
-        return quantity * product.price * (1 - discount)
+import promotions
 
 
 # Updated Product Class
@@ -84,7 +48,7 @@ class Product:
 
     def set_promotion(self, promotion):
         self.promotion = promotion
-        print(f"Promotion '{promotion.name}' applied to {self.name}.")
+        # print(f"Promotion '{promotion.name}' applied to {self.name}.")
 
     def buy(self, quantity):
         if quantity > self.quantity:
@@ -94,13 +58,15 @@ class Product:
 
         if self.promotion:
             total_price = self.promotion.apply_promotion(self, quantity)
+            promotion_name = self.promotion.name
         else:
             total_price = quantity * self.price
+            promotion_name = "No promotion"
 
         if self.quantity == 0:
             self.active = False
 
-        return f"Total price for {quantity} units of {self.name} with ({self.promotion.name}): {total_price}"
+        return f"Total price for {quantity} units of {self.name} with ({promotion_name}): {total_price}"
 
 
 # NonStockedProduct Class
@@ -146,30 +112,31 @@ class LimitedProduct(Product):
         # Decrease stock based on the purchase
         self.set_quantity(self.quantity - quantity)
 
-        return super().buy(quantity)
+        return f"Total price for {quantity} units of {self.name}: {total_price}"
 
 #
-# # setup initial stock of inventory
+# setup initial stock of inventory
 # product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
 #                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
 #                 Product("Google Pixel 7", price=500, quantity=250),
 #                 NonStockedProduct("Windows License", price=125),
-#                 LimitedProduct("Shipping", price=10, quantity=250, maximum=10)
+#                 LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
 #                ]
-#
-# # Create promotion catalog
-# second_half_price = SecondHalfPrice("Second Half price!")
-# third_one_free = ThirdOneFree("Third One Free!")
-# thirty_percent = PercentDiscount("30% off!", percent=30)
-#
-# # Add promotions to products
-# product_list[0].set_promotion(second_half_price)
-# product_list[1].set_promotion(third_one_free)
-# product_list[3].set_promotion(thirty_percent)
-# product_list[4].set_promotion(second_half_price)
 
-# print(product_list[0].buy(20))
+# # # Create promotion catalog
+# second_half_price = promotions.SecondHalfPrice("Second Half price!")
+# third_one_free = promotions.ThirdOneFree("Third One Free!")
+# thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+# #
+# # # Add promotions to products
+# # product_list[0].set_promotion(second_half_price)
+# product_list[1].set_promotion(third_one_free)
+# # product_list[3].set_promotion(thirty_percent)
+# # product_list[4].set_promotion(second_half_price)
+# #
+# # print(product_list[0].buy(20))
 # print(product_list[1].buy(30))
-# print(product_list[3].buy(20))
-# print(product_list[4].buy(10))
-# print(product_list[4].buy(9))
+# # print(product_list[3].buy(20))
+# # print(product_list[4].buy(10))
+# # print(product_list[4].buy(9))
+# print(product_list[0].buy(87))
